1b754da59a82278e0d07b5ce026d2128c6e387b2
IainLR <iainrobertzon@gmail.com>
2021-01-04 21:00:22 -0600

    redux established

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,46 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
+// import axios from 'axios'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
+  // const [products, setProducts] = useState([])
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      // const res = await axios.get('/api/products')
-      const { data } = await axios.get('/api/products')
+    // const fetchProducts = async () => {
+    //   // const res = await axios.get('/api/products')
+    //   const { data } = await axios.get('/api/products')
 
-      setProducts(data)
-    }
+    //   setProducts(data)
+    // }
 
-    fetchProducts()
-  }, [])
+    // fetchProducts()
+
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
       <h1>Latest Pedals</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
