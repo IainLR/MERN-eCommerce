c0b3796538d5eb423a8a62e6a3f446c3869d1428
IainLR <iainrobertzon@gmail.com>
2021-01-09 13:36:31 -0600

    searchbar

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -22,13 +22,13 @@ import {
 
 // functions much like a useEffect or componentDidMount
 // async (dispatch) is a perk of thunk
-export const listProducts = () => async (dispatch) => {
+export const listProducts = (searchTerm = '') => async (dispatch) => {
   try {
     dispatch({
       type: PRODUCT_LIST_REQUEST,
     })
 
-    const { data } = await axios.get('/api/products')
+    const { data } = await axios.get(`/api/products?keyword=${searchTerm}`)
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
